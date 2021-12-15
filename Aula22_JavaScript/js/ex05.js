392f577837f2eba4c5e420f3e16f11b9d307d137
PietraValentina <pcanesso@gmail.com>
2021-12-15 19:51:45 -0300

    Atualização geral

--- Aula22_JavaScript/js/ex05.js
+++ Aula22_JavaScript/js/ex05.js
@@ -1,9 +1,11 @@
-var n1, n2, soma
+var n1, n2, soma, sub, mult, div
 n1=prompt("Digite o primeiro valor: ")
 n2=prompt("Digite o segundo valor: ")
 soma=parseInt(n1)+parseInt(n2)
-
-alert("A soma é: " + soma)
+sub= parseInt(n1)-parseInt(n2)
+mult= parseInt(n1)* parseInt(n2)
+div= parseInt(n1)/parseInt(n2)
+alert("A soma é: " + soma + "\nA subtração é: " + sub +"\nA multiplicação é: " + mult + "\nA divisão é: " + div)
 
 
 
